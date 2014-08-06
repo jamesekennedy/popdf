c89464aa2beff7f1c0b5da0971e894e24bccc327
James Kennedy <james@jameskennedy.ie>
2014-08-06 14:40:27 +0100

    Set up default host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,9 @@ Pdfpo::Application.configure do
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
 
-  
+
   config.action_mailer.postmark_settings = {:api_key => "cecb87dc-569f-42cf-a749-38bd79ab0953"}
   config.action_mailer.delivery_method = :postmark
+
+  config.action_mailer.default_url_options  = {:host => "www.freepurchaseordertemplate.com"}
 end
